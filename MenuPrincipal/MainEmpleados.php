6b4ff36671f96c6e12966e70759debf611d66b3a
Victor M Rendon Local <vitorren51@gmail.com>
2021-10-22 02:50:01 -0500

    Se agrega seccion de Empleados

--- MenuPrincipal/MainEmpleados.php
+++ MenuPrincipal/MainEmpleados.php
@@ -3,51 +3,79 @@
             <div id="layoutSidenav_content">
                 <main>
                         <div class="container-fluid px-4">
-
 <!-- Inicio del formulario-->
+
 <?php
 //require "../scrips/SesionActiva.php"; 
-if($NivelAccesoActivo==1 || $NivelAccesoActivo==2) { $where="";}
-else if($NivelAccesoActivo==4)
-{ $where="where idUsuario = $idUsuario"; }
-$sql= "select * from usuarios $where";
+// if($NivelAccesoActivo==1 || $NivelAccesoActivo==2) { $where="";}
+// else if($NivelAccesoActivo==4)
+// { $where="where idUsuario = $idUsuario"; }
+$where="";
+$sql= "select * from empleados $where";
 $resultado = $mysqli->query($sql);
 ?>
 
 <link rel="stylesheet" href="../css/bootstrap.min.css" >
 <!--<link rel="stylesheet" href="../Fondos/basico.css">-->
     <div class="container text-center">
-        <h1 class="text-center mt-3 display-3 font-weight-bold">Alta de nuevo usuario</h1>
+        <h1 class="text-center mt-3 display-3 font-weight-bold">Alta de nuevo empleado</h1>
        <div class="rows">
            <div class="col">
 
                 <div class="shadow-lg p-3 mb-5 mt-5 bg-body rounded  w-50 mx-auto" style="background-color: rgb(255, 255, 255);">
-                    <form class=" needs-validation "  novalidate method="post" action=""> <!-- Formulario -->
+                    <form class=" needs-validation "  novalidate method="POST" action=""> <!-- Formulario -->
 
                         <!-- No se muestra el imput, solo es para recibir y mandar el id del usuario-->
-                        <input type="hidden" id="idUsuario" name="idUsuario" value="1">
+                        <input type="hidden" id="idEmpleado" name="idEmpleado" value="1">
                         
                         <div class="form-group w-75 mx-auto"> <!-- Nombre-->
-                            <label for="Nombre" class="label font-weight-bold">Nombre de usuario:</label>
-                            <input type="text" class="form-control" id="Nombre" name="Nombre" placeholder="Nombre(s) Apellidos" autofocus required>
+                            <label for="Nombre" class="label font-weight-bold">Nombre(s):</label>
+                            <input type="text" class="form-control" id="NombreCliente" name="Nombre" placeholder="Juan Pablo" autofocus required>
                             <div class="valid-feedback">Verificado</div>
-                            <div class="invalid-feedback">Ingrese su nombre de usuario.</div>
+                            <div class="invalid-feedback">Ingrese un nombre válido.</div>
                         </div>
 
-                        <div class="form-group w-75 mx-auto"> <!-- Email -->
-                            <label for="Email" class="label font-weight-bold">Correo Electronico:</label>
-                            <input type="email" class="form-control" id="Email" name="Email" placeholder="usuario@tiendavideo.com" required>
+                        <div class="form-group w-75 mx-auto"> <!-- Apellidos -->
+                            <label for="Nombre" class="label font-weight-bold">Apellidos:</label>
+                            <input type="text" class="form-control" id="ApellidosCliente" name="Apellidos" placeholder="Hernandez Garcia" autofocus required>
                             <div class="valid-feedback">Verificado</div>
-                            <div class="invalid-feedback">Ingrese su correo.</div>
-                        </div>
-                        
-                        <div class="form-group w-50 mx-auto"> <!-- Pasword -->
-                            <label for="contrasena" class="font-weight-bold">Contraseña:</label>
-                            <input type="password" class="form-control" id="idCliente" name="idCliente" placeholder="Ej3mpl01234" required>
-                            <div class="valid-feedback">Verificado</div>
-                            <div class="invalid-feedback">Ingrese su contraseña</div>
+                            <div class="invalid-feedback">Ingrese apellidos válido.</div>
                         </div>
 
+                        <div class="form-group w-75 mx-auto"> <!-- Fecha Nacimiento -->
+                            <label for="Fecha de Nacimiento" class="label font-weight-bold">Fecha de Nacimiento:</label>
+                            <input type="date" class="form-control" id="Fecha de Nacimiento" name="Fecha de Nacimiento">
+                            <div class="valid-feedback">Verificado</div>
+                            <div class="invalid-feedback">Fecha inválida.</div>
+                        </div>
+
+                        <div class="form-group w-75 mx-auto"> <!-- Sexo -->
+                            <label for="Sexo" class="label font-weight-bold">Sexo:</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="Masculino" id="SexoMas" value="M" checked>
+                                <label class="form-check-label" for="flexRadioDefault1"> Masculino </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="Femenino" id="SexoFem" value="F">
+                                <label class="form-check-label" for="flexRadioDefault1"> Femenino </label>
+                            </div>
+                        </div>
+
+                        <div class="form-group w-75 mx-auto"> <!-- Telefono -->
+                            <label for="Telefono" class="label font-weight-bold">Teléfono:</label>
+                            <input type="tel" class="form-control" id="Telefono" name="Telefono" placeholder="8671224498" required maxlength="10">
+                            <div class="valid-feedback">Verificado</div>
+                            <div class="invalid-feedback">Número inválido.</div>
+                        </div>
+
+                        <div class="form-group h-75 mx-auto"><!-- Direccion -->
+                            <label for="Direccion" class="label font-weight-bold">Dirección:</label>
+                            <input type="text" class="form-control" id="Direccion" name="Direccion" placeholder="C. Palmera 5554, Col. El Nogal" required>
+                            <div class="valid-feedback">Verificado</div>
+                            <div class="invalid-feedback">Ingrese una dirección válida.</div>
+                        </div>
+
+                        <!-- -->
                         <div class="w-50 mx-auto"> <!-- Caja de opciones -->
                             <label for="Nivel de Acceso" class="font-weight-bold">Nivel de Acceso:</label>
                             <select name="NivelAcc" id="NivelAccesso" class="custom-select form-select-lg mb-2" required>
@@ -60,7 +88,7 @@ $resultado = $mysqli->query($sql);
                             <div class="valid-feedback">Verificado</div>
                             <div class="invalid-feedback">Escoja una opción.</div>
                         </div>
-
+                        
                         <div class="form-group text-center"> <!-- Btn -->
                             <button class="mt-3 btn btn-success btn-lg font-weight-bold" type="submit" id="Enviar" name="Enviar">Enviar</button>
                         </div>
@@ -70,8 +98,9 @@ $resultado = $mysqli->query($sql);
              </div>
         </div>
     </div>
-    <!--Tabla-->
-    <div id="layoutSidenav_content" class="shadow-lg p-2 mb-5 mt-5 bg-body rounded  w-100 mx-auto">
+
+<!--Tabla-->
+    <div id="layoutSidenav_content" class="shadow-lg p-3 mb-5 mt-5 bg-body rounded  w-100 mx-auto">
                 <main>
                     <div class="container-fluid">
                         <h1 class="mt-1 mb-4 text-center">Registros</h1>
@@ -87,35 +116,44 @@ $resultado = $mysqli->query($sql);
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
-                                Usuarios registrados
+                                Empleados registrados
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple">
                                     <thead><!-- Inicio de la tabla-->
                                         <tr>
-                                            <th>idUsuario</th>
+                                            <th>idEmpleado</th>
                                             <th>Nombre</th>
-                                            <th>Email</th>
-                                            <th>Contrasena</th>
+                                            <th>Apellidos</th>
+                                            <th>FechaNacimiento</th>
+                                            <th>Sexo</th>
+                                            <th>Telefono</th>
+                                            <th>Direccion</th>
                                             <th>Nivel Acceso</th>
                                         </tr>
                                     </thead>
                                     <tfoot> <!-- Pie de la tabla-->
                                         <tr>
-                                            <th>idUsuario</th>
+                                            <th>idEmpleado</th>
                                             <th>Nombre</th>
-                                            <th>Email</th>
-                                            <th>Contrasena</th>
+                                            <th>Apellidos</th>
+                                            <th>FechaNacimiento</th>
+                                            <th>Sexo</th>
+                                            <th>Telefono</th>
+                                            <th>Direccion</th>
                                             <th>Nivel Acceso</th>
                                         </tr>
                                     </tfoot>
                                     <tbody>
                                         <?php while($row= $resultado->fetch_assoc()) { ?>
                                             <tr>
-                                                <td><?php echo $row['idUsuario']?></td>
+                                                <td><?php echo $row['idEmpleado']?></td>
                                                 <td><?php echo $row['Nombre']?></td>
-                                                <td><?php echo $row['Email']?></td>
-                                                <td><?php echo $row['Contrasena']?></td>
+                                                <td><?php echo $row['Apellidos']?></td>
+                                                <td><?php echo $row['FechaNacimiento']?></td>
+                                                <td><?php echo $row['Sexo']?></td>
+                                                <td><?php echo $row['Telefono']?></td>
+                                                <td><?php echo $row['Direccion']?></td>
                                                 <td><?php echo $row['NivelAcceso']?></td>
                                             </tr>
                                        <?php }?>
@@ -127,6 +165,7 @@ $resultado = $mysqli->query($sql);
                 </main>
                 
     </div>
+
     <script src="../js/jquery-3.5.1.slim.min.js" ></script>
     <script src="../js/bootstrap.bundle.min.js" ></script>
     <script> //scrip para validar campos
@@ -151,9 +190,7 @@ $resultado = $mysqli->query($sql);
                 })
             })()
     </script>
-
 <!-- Fin del formulario-->
-
                         </div>
                 </main>
     
--- PartesMenu/ParteSuperior.php
+++ PartesMenu/ParteSuperior.php
@@ -19,7 +19,7 @@
 
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!--Barra de navegacion-->
-            <a class="navbar-brand ps-3" href="index.html">Sistema de ventas</a>
+            <a class="navbar-brand ps-3" href="principal.php">Sistema de ventas</a>
             <!-- Icono para desplegar barra lateral-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             
@@ -54,22 +54,9 @@
                                 </a> -->
                                 
                                 
-                                <div> <!-- -->
-                                    <div class="sb-sidenav-menu-heading">Secciones</div>
-                                    <!-- 
-                                        Seccion para cambiar el color
-                                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                        Layouts
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                            <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                        </nav>
-                                    </div>
-                                </div>-->
+                                <!-- -->
+                                <div class="sb-sidenav-menu-heading">Secciones</div>
+                                    
                                 <?php //Para mostrar secciones segun el nivel de acceso
                                         if($NivelAccesoActivo==1 || $NivelAccesoActivo==2){
                                 ?>
@@ -79,23 +66,18 @@
                                         <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                                         Registros
                                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    
-                                    
-
+                                    </a>   
                                     <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                                         <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                                Usuarios
+                                            
+                                            
+                                                <!--Apaprtados inicio-->
                                                 <a class="nav-link" href="../MenuPrincipal/MainUsuarios.php">Usuarios</a>
-                                            </a>
-                                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                                <nav class="sb-sidenav-menu-nested nav">
-                                                    <a class="nav-link" href="login.html">Login</a>
-                                                    <a class="nav-link" href="register.html">Register</a>
-                                                    <a class="nav-link" href="password.html">Forgot Password</a>
-                                                </nav>
-                                            </div>
+                                                <a class="nav-link" href="../MenuPrincipal/MainEmpleados.php">Empleados</a>
+                                                <!--Apaprtados inicio-->
+                                            
+                                            
+                                            <!-- Seccion de errores
                                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
                                                 Error
                                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
@@ -104,7 +86,7 @@
                                                 <nav class="sb-sidenav-menu-nested nav">
                                                     <a class="nav-link" href="401.html">401 Page</a>                                
                                                 </nav>
-                                            </div>
+                                            </div>-->
                                         </nav>
                                     </div>
                                 </div>
