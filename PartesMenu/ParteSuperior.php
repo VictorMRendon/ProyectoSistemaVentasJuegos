cd0f240c266864c7f6db85c47d61666186463e6c
RENUXA <l19100254@nlaredo.tecnm.mx>
2021-12-01 16:09:35 -0600

    acabaaaaaaa

--- PartesMenu/ParteSuperior.php
+++ PartesMenu/ParteSuperior.php
@@ -36,8 +36,8 @@
                     <i class="fas fa-user fa-fw"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <!-- <li><a class="dropdown-item" href="#!">Configuración</a></li>
-                        <li><hr class="dropdown-divider" /></li> -->
+                        <li><a class="dropdown-item" href="#!">Configuración</a></li>
+                        <li><hr class="dropdown-divider" /></li>
                         <li><a class="dropdown-item" href="../scrips/LoginOut.php">Salir</a></li>
                     </ul>
                 </li>
