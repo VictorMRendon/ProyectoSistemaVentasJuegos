d37982486dace915b3036ea920671f6df4e1300c
RENUXA <l19100254@nlaredo.tecnm.mx>
2021-12-01 15:12:54 -0600

    creo que ya

--- MenuPrincipal/MainGraficas.php
+++ MenuPrincipal/MainGraficas.php
@@ -101,7 +101,7 @@
                         titulo.push(data[i][1]);
                         cantidad.push(data[i][4]);//2
                         colores.push(colorRGB());
-                    }//Stock de los productos
+                    }
                     CrearGrafico(titulo,cantidad,colores,'bar','GRAFICO EN BARRAS DE LOS PRODUCTOS','graficobar');
 
                 //} 
@@ -122,7 +122,7 @@
                         titulo.push(data[i][1]);
                         cantidad.push(data[i][7]);//2
                         colores.push(colorRGB());
-                    }                                       //horizontalBar costo de los productos
+                    }                                       //horizontalBar
                     CrearGrafico(titulo,cantidad,colores,'doughnut','GRAFICO COSTO DE LOS PRODUCTOS','graficodoughnut');
 
                 //}
@@ -144,7 +144,7 @@
                         titulo.push(data[i][1]);
                         cantidad.push(data[i][4]);//2
                         colores.push(colorRGB());
-                    }//Stock
+                    }
                     CrearGrafico(titulo,cantidad,colores,'pie','GRAFICO EN PIE DE LOS PRODUCTOS','graficopie');
 
                 //} 
