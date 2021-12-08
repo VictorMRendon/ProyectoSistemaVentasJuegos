ac103316c940e63f759d198ac910e36b951c58d5
Victor M Rendon Local <vitorren51@gmail.com>
2021-12-08 10:11:06 -0600

    intento 3 de hots

--- scrips/conexion.php
+++ scrips/conexion.php
@@ -1,6 +1,9 @@
 <?php 
 
-$mysqli = new mysqli("localhost","root", "", "bdtiendajuegos");
+//$mysqli = new mysqli("localhost","root", "", "bdtiendajuegos");
+//$mysqli = new mysqli("localhost","id18059824_renesalinas", "ReneSalinas250501!", "id18059824_proyectosistemaventasjuegos");
+//$mysqli = new mysqli("proyectosistemaventasjuegosserver.database.windows.net","ReneSalinas", "ReneVictorVela25", "ProyectoSistemaVentasJuegos");
+$mysqli = new mysqli("sql109.epizy.com","epiz_30546548", "AuxsK0ZswVwCoR", "epiz_30546548_bdtiendajuegos");
 
 /* $NombreBD="bdtiendajuegos";
 $Usuario="root";
